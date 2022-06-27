930f09e676ee0d1e96daa0d2959cfcf476550b94
Francisco Berwart <58098908+Panchober27@users.noreply.github.com>
2022-06-27 00:47:30 -0400

    avanzo con mantenedor ejemplares

--- src/containers/Libros/index.jsx
+++ src/containers/Libros/index.jsx
@@ -4,7 +4,7 @@ import {
   getEjemplares,
   clearEjemplaresState,
 } from '../../redux/reducers/ejemplaresReducer';
-import { Button, Drawer, Row, Col, Tooltip, Modal } from 'antd';
+import { Button, Drawer, Row, Col, Tooltip, message } from 'antd';
 // importar moment para trabajar con fechas
 import moment from 'moment';
 import {
@@ -12,6 +12,8 @@ import {
   SearchableTable,
   CustomModal,
   InputField,
+  SelectField,
+  DatePickerField,
 } from '../../components/common';
 import swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
@@ -23,24 +25,76 @@ const Books = () => {
   const MySwal = withReactContent(swal);
   const history = useHistory();
 
-  // Hook de estado: Mostrar/ocultar el Drawer para editar/darDeBaja/darDeAlta un ejemplar.
+  // Hook de estado: Mostrar/ocultar el Drawer para editar/darDeBaja/darDeAlta un Libro.
   const [drawerVisible, setDrawerVisible] = useState(false);
+
+  // Hook de estado: Mostrar/ocultar el Modal para editar/darDeBaja/darDeAlta un ejemplar.
+  const [modalVisible, setModalVisible] = useState(false);
 
   // Hook de estado: Ejemplar a crear/editar. en caso de editar puede ser la cantidad de ejemplares disponibles!
   const [isEdit, setIsEdit] = useState(false);
+
+  // Hook de estado: Libro a crear/editar.
+  const [libroData, setLibroData] = useState({});
+
+  // Definicion Columnas tabla Libros.
+  const librosColumns = [
+    {
+      title: 'Titulo',
+      dataIndex: 'nombre',
+      key: 'nombre',
+      width: '30%',
+    },
+    {
+      title: 'Ejemplares',
+      dataIndex: 'autor',
+      key: 'autor',
+      width: '30%',
+    },
+    {
+      title: 'Stocks',
+      dataIndex: 'stock',
+      key: 'stock',
+      width: '30%',
+    },
+  ];
+
+  // Opciones de la tabla de libros.
+  const librosOptions = [
+    {
+      title: 'Crear Nuevo Libro',
+      onClick: () => {
+        setDrawerVisible(true);
+      },
+    },
+  ];
 
   // Funcion para renderizar el drawer de edicion/creacion de ejemplares.
   const renderDrawerForm = () => (
     <Drawer
       title='Editar Ejemplar'
       placement='right'
-      closeOnClick={() => setDrawerVisible(false)}
-      onClose={() => setDrawerVisible(false)}
+      destroyOnClose={true}
+      closeOnClick={() => {
+        setLibroData({});
+        setDrawerVisible(false);
+      }}
+      onClose={() => {
+        setLibroData({});
+        setDrawerVisible(false);
+      }}
       visible={drawerVisible}
       width={500}
       footer={
-        <div>
-          <Button type='primary' onClick={() => setDrawerVisible(false)}>
+        <div style={{ display: 'flex', justifyContent: 'right' }}>
+          <Button
+            type='danger'
+            style={{ marginRight: '10px' }}
+            onClick={() => {
+              setLibroData({});
+              setDrawerVisible(false);
+            }}
+          >
             Cancelar
           </Button>
           <Button
@@ -71,40 +125,160 @@ const Books = () => {
         ) : (
           <>
             <Row>
-              <Col span={24}>CREACION!</Col>
+              <Col span={12}>
+                <InputField
+                  label='Titulo'
+                  name='title'
+                  type='text'
+                  required
+                  onChange={(value) => {
+                    setLibroData({ ...libroData, nombre: value });
+                  }}
+                />
+              </Col>
+              <Col span={12}>
+                <SelectField
+                  label='Tipo'
+                  name='insbnTipo'
+                  data={[
+                    { value: '1', title: 'Tapa Blanda' },
+                    { value: '2', title: 'Tapa Dura' },
+                    { value: '3', title: 'De Bolsillo' },
+                  ]}
+                  required
+                  onChange={(value) => {
+                    setLibroData({ ...libroData, isbnTipo: value });
+                  }}
+                />
+              </Col>
+              <Col span={12}>
+                <InputField
+                  label='Editorial'
+                  name='editorial'
+                  type='text'
+                  required
+                  onChange={(value) => {
+                    setLibroData({ ...libroData, editorial: value });
+                  }}
+                />
+              </Col>
+              <Col span={12}>
+                <InputField
+                  label='Edición'
+                  name='edicion'
+                  type='text'
+                  required
+                  onChange={(value) => {
+                    setLibroData({ ...libroData, edicion: value });
+                  }}
+                />
+              </Col>
+
+              <Col span={12}>
+                <DatePickerField
+                  label='Fecha de Publicación'
+                  name='fechaPublicacion'
+                  required
+                  onChange={(value) => {
+                    setLibroData({ ...libroData, fechaPublicacion: value });
+                  }}
+                />
+              </Col>
+
+              <Col span={12}>
+                <Button
+                  type='primary'
+                  onClick={() => {
+                    setModalVisible(true);
+                  }}
+                >
+                  Agregar Ejemplares
+                </Button>
+              </Col>
             </Row>
+            <Button
+              type='primary'
+              onClick={() => {
+                console.log(libroData);
+              }}
+            >
+              ver libro data.
+            </Button>
           </>
         )}
-
-        <Row>
-          <Col span={24}>
-            <InputField
-              label='Titulo'
-              name='title'
-              placeholder='Titulo'
-              type='text'
-              required
-            />
-          </Col>
-        </Row>
       </>
     </Drawer>
+  );
+
+  // Funcion para renderizar el modal para crear o editar ejemplares de un libro.
+  const renderModalForm = () => (
+    <CustomModal
+      title='Crear Ejemplar'
+      visible={modalVisible}
+      onClose={() => {
+        setModalVisible(false);
+      }}
+      footer={
+        <div style={{ display: 'flex', justifyContent: 'right' }}>
+          <Button
+            type='danger'
+            style={{ marginRight: '10px' }}
+            onClick={() => {
+              setModalVisible(false);
+            }}
+          >
+            Cancelar
+          </Button>
+          <Button
+            type='primary'
+            onClick={() => {
+              setModalVisible(false);
+              MySwal.fire({
+                title: 'Ejemplar creado con éxito',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }}
+          >
+            Guardar
+          </Button>
+        </div>
+      }
+    >
+      <>
+        <Row>
+          <Col span={12}>Datos del ejemplar</Col>
+        </Row>
+      </>
+    </CustomModal>
   );
 
   return (
     <>
       <h1 style={{ textAlign: 'center' }}>Mantenedor de Libros-Ejemplares</h1>
 
-      <Button
-        type='primary'
-        onClick={() => {
-          setDrawerVisible(!drawerVisible);
-        }}
-      >
-        mostrar/ocultar drawer
-      </Button>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 col-md-4'>
+            <SearchableTable
+              columns={librosColumns}
+              menuOptions={librosOptions}
+              dataSource={[]}
+              rowKey='id'
+              pagination={false}
+              onChange={(pagination, filters, sorter) => {
+                console.log('params', pagination, filters, sorter);
+              }}
+            />
+          </div>
+          <div className='col-12 col-md-4'>TABLA DE REVISTAS</div>
+          <div className='col-12 col-md-4'>TABLA DE TRABAJOS</div>
+        </div>
+      </div>
 
       {renderDrawerForm()}
+      {renderModalForm()}
     </>
   );
 };
