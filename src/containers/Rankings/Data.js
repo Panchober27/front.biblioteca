e078a772978447215ef6ec7485487b1724992105
Francisco Berwart <58098908+Panchober27@users.noreply.github.com>
2022-07-16 05:46:37 -0400

    agrego graficos con datos falsos

---
+++ src/containers/Rankings/Data.js
@@ -0,0 +1,26 @@
+export const UserData = [
+  {
+    id: 1,
+    year: 2016,
+    userGain: 80000,
+    userLost: 823,
+  },
+  {
+    id: 2,
+    year: 2017,
+    userGain: 45698,
+    userLost: 1250,
+  },
+  {
+    id: 3,
+    year: 2018,
+    userGain: 95020,
+    userLost: 385,
+  },
+  {
+    id: 4,
+    year: 2019,
+    userGain: 50125,
+    userLost: 754,
+  },
+];
--- src/containers/Rankings/index.jsx
+++ src/containers/Rankings/index.jsx
@@ -1,8 +1,28 @@
 import React, { Fragment, useState, useEffect, Row, Col } from "react";
 import { Collapse } from "antd";
-
+import { UserData } from "./Data";
+import { BarChart, LineChart, PieChart } from "../../components/charts";
 const Rankings = () => {
   const { Panel } = Collapse;
+
+  const [userData, setUserData] = useState({
+    labels: UserData.map((user) => user.year),
+    datasets: [
+      {
+        label: "Users Gain",
+        data: UserData.map((user) => user.userGain),
+        // data: UserData.map((user) => user.userLost),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+    ],
+  });
 
   return (
     <>
@@ -20,12 +40,24 @@ const Rankings = () => {
               <p className="card-text">
                 Probar grafico de barras sin librería. <br />
                 el grafico se encuantra en este link. <br />
-                <a href="https://www.youtube.com/watch?v=YaZJ3alT2no" target="_blank">
-                  Tutorial
-                </a>
               </p>
             </div>
           </div>
+          <br />
+
+
+          <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+            <div style={{ width: "500px" }}>
+              <BarChart chartData={userData} />
+            </div>
+            <div style={{ width: "500px" }}>
+              <LineChart chartData={userData} />
+            </div>
+            <div style={{ width: "500px" }}>
+              <PieChart chartData={userData} />
+            </div>
+          </div>
+
         </Panel>
 
         <Panel
